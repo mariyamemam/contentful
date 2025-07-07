1629934a7f99db6de0aadbd7b90b1479918fce96
Mariyam Emamudeen <mariyam.emamudeen@experionglobal.local>
2025-07-07 11:10:46 +0530

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,6 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 if (process.env.NODE_ENV === 'development' && window.self === window.top) {
-  // You can remove this if block before deploying your app
   root.render(<LocalhostWarning />);
 } else {
   root.render(
