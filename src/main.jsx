5712a37b0a1578458af2ffc5d60ea1d5b4ff28cf
Mariyam Emamudeen <mariyam.emamudeen@experionglobal.local>
2025-07-07 10:58:07 +0530

    rename index

--- src/main.jsx
+++ src/main.jsx
@@ -10,14 +10,9 @@ import App from './App';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-if (process.env.NODE_ENV === 'development' && window.self === window.top) {
-  // You can remove this if block before deploying your app
-  root.render(<LocalhostWarning />);
-} else {
-  root.render(
-    <SDKProvider>
-      <GlobalStyles />
-      <App />
-    </SDKProvider>
-  );
-}
+root.render(
+  <SDKProvider>
+    <GlobalStyles />
+    <App />
+  </SDKProvider>
+);
