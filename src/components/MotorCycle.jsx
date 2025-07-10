5fe070e7757f2f0f36a1c2247059f106f697724e
Mariyam Emamudeen <mariyam.emamudeen@experionglobal.local>
2025-07-10 14:54:13 +0530

    feature view bikes

---
+++ src/components/MotorCycle.jsx
@@ -0,0 +1,55 @@
+import client from '.././contentfulClient'
+import { useState, useEffect } from 'react';
+import './styles.css'
+
+export default function MotorCycle(){
+    
+    const [entries,setEntries] = useState([]);
+    const [category,setCategory] = useState([]);
+
+    useEffect(() => {
+        client.getEntries({
+            content_type:'motorcycleCategory'
+        })
+        .then((response) => {
+                setEntries(response.items);
+            })
+            .catch(console.error);
+    }, []);
+
+    function selectCategory(categoryinp){
+        setCategory(categoryinp)
+    }
+
+    return(
+        <div className="motorcyclelist-container">
+            <div className="motorcycle-category-list">
+            {entries.map(
+            (entry) => (
+                <button onClick={()=>{selectCategory(entry.fields.motorcycles)}} key={entry.sys.id}>{entry.fields.categoryTitle}</button>
+            )
+            )}
+            </div>
+            <div className="motorcycle-view">
+                {category.length > 0 &&
+                    category.map((motorcycle) => (
+                    <div key={motorcycle.sys.id} className='motorcycle-view__card'>
+                        {console.log(category)}
+                        {motorcycle.fields.image.map((img)=>(
+                            <div className="images" key={img.sys.id}>
+                                <img src={img.fields.file.url}/>
+                            </div>
+                        ))}
+                        {console.log(category)}
+                        <div className="motorcycle-desc"> 
+                            <p>{motorcycle.fields.model}</p>
+                            <p>Start Price:{motorcycle.fields.startPrice}$</p>
+                            </div>
+                    </div>
+                    ))
+                }
+            </div>
+            <hr className="hr-line"/>
+        </div>
+    );
+}
